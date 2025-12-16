efd8575d1792649309e0bad23772ff92bf6ec7b7
Geoffrey Green Jr <geoffreyg3655@gmail.com>
2025-12-15 22:00:51 -0700

    Complete Project 8: Popup classes, Section refactor, TodoCounter integration

---
+++ components/PopupWithForm.js
@@ -0,0 +1,31 @@
+import Popup from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super({ popupSelector });
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = Array.from(
+      this._popupForm.querySelectorAll(".popup__input")
+    );
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+    this._inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+}
+
+export default PopupWithForm;
